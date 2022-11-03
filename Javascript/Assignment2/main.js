0c6348da172599156ae9faaf55a0aa18f39f761c
rei-kaji <st.r.z.rei@gmail.com>
2022-11-02 23:22:46 -0700

    finishe assignment2

---
+++ Javascript/Assignment2/main.js
@@ -0,0 +1,24 @@
+const scrollUp = document.getElementById("arrow");
+const scrollDown = document.getElementById("down");
+const galleryImage = document.getElementById("gallery");
+const Images = [
+  "./assets/photo1.png",
+  "./assets/photo2.png",
+  "./assets/photo3.png",
+  "./assets/photo4.png",
+];
+
+scrollUp.addEventListener("click", () => {
+  alert("Hello Lion!");
+});
+
+scrollDown.addEventListener("click", () => {
+  scrollTo({ top: 1000 });
+});
+
+let image = "";
+for (let i = 0; i < Images.length; i++) {
+  a += `<img src=${Images[i]}>`;
+}
+
+galleryImage.innerHTML = image;
