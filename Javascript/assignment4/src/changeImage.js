2f1bca1d2b3fce8fda0d2c54f4ca3531be16a9ce
rei-kaji <st.r.z.rei@gmail.com>
2022-11-17 20:09:42 -0800

    deleted file

--- Javascript/assignment4/src/changeImage.js
+++
@@ -1,8 +0,0 @@
-const over_big = require("../assets/over_big.png");
-
-const image = document.getElementsByClassName("weatherImage");
-
-image.addEventListener("mouseover", () => {
-  console.log("Hi");
-  image.src = over_big;
-});
